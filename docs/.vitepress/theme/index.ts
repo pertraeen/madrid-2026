7132d31e89e16c67985a0d8312b175b54d3e411f
Per Eilif Træen <pereilif@gmail.com>
2026-07-05 17:48:48 +0200

    Widget for weather information

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,13 +2,14 @@ import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import Countdown from '../../../components/countdown.vue'
 import Currency from '../../../components/currency.vue'
+import Weather from '../../../components/weather.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       'home-hero-info-after': () => h(Countdown),
-      'home-features-after': () => h(Currency)
+      'home-features-after': () => h('div', [h(Weather), h(Currency)])
     })
   }
 }
